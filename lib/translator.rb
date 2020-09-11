89392f853827da75fd19416e49c727c4b6e403e0
masterful-principle-0059 <william.neal83@gmail.com>
2020-09-11 15:10:58 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,35 @@
+require 'yaml'
+require 'pry'
 # require modules here
 
-def load_library
+def load_library(path)
+  helper = YAML.load_file(path)
+  translated = {}
+  helper.each do |name, array|
+    translated[name] = {
+      :english => array[0],
+      :japanese => array[1]
+      # binding.pry
+    }
+    # binding.pry
+  end
+  translated
+  # binding.pry
+end
+
+def get_japanese_emoticon(path, emoticon)
+  helper = load_library(path)
+  translated = ""
+  helper.each do |eng, hash|
+    hash.each do |k, v|
+      # binding.pry
+    end
+    binding.pry
+  end
   # code goes here
 end
 
-def get_japanese_emoticon
-  # code goes here
-end
+def get_english_meaning(path, emoticon)
 
-def get_english_meaning
   # code goes here
 end
